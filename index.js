c14238c8da85a519d1b4d00945dab69069f5f7ee
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-06-19 21:00:56 +0530

    Add client side and server side sockets

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require('express')()
 const http = require('http').Server(server)
 const path = require('path')
+const io = require('socket.io')(http)
 
 http.listen(3000, () => {
   console.log('listening on port 3000')
@@ -8,4 +9,8 @@ http.listen(3000, () => {
 
 server.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
 })
