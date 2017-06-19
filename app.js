18fd96e34fcd45260d06d1485f567f055d778026
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-06-19 12:18:59 +0530

    Add code to listen messages from clients

--- app.js
+++ app.js
@@ -10,8 +10,29 @@ server.listen(3000, () => {
 })
 
 // creates a websocket server
-const ws = require('websocket').server
+const WS = require('websocket').server
 
-const wsServer = new ws({
+const wsServer = new WS({
   httpServer: server
+})
+
+let count = 0
+let clients = {}
+
+// Listening for connections
+wsServer.on('request', (req) => {
+  // Accept the connection
+  const connection = req.accept('echo-protocol', req.origin)
+  let id = count++
+  clients[id] = connection
+  console.log(`Connection accepted ${id}`)
+  connection.on('message', (message) => {
+    // string message sent to server
+    const msgString = message.utf8data
+
+    // loop through all the clients
+    for (var i in clients) {
+      clients[i].sendUTF(msgString)
+    }
+  })
 })
