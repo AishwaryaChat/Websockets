bdafc211c00f6c32759b61c0aa1f76e97234c7e0
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-06-20 22:05:27 +0530

    DO the following 1. Separate folder for vies on server side 2. Server signup file through a get request to server.

--- server.js
+++ server.js
@@ -7,31 +7,33 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const {obj} = require('./config.js')
 
+const controllers = require('./controllers')
+
 server.use(bodyParser.json(''))
 server.use(bodyParser.urlencoded({extended: true}))
-server.use(express.static(path.join(__dirname, '/public/css')))
-server.use('/images', express.static(path.join(__dirname, '/public/images')))
-server.use(express.static(path.join(__dirname, '/public/html')))
+server.use(express.static(path.join(__dirname, '/public')))
 
 // Database connection
 mongoose.connect(`mongodb://${obj.hostName}:${obj.portNumber}/${obj.dbName}`)
 const db = mongoose.connection
-db.on('error', (err) => console.log('Error occured', err))
+db.on('something', (err) => console.log('Error occured', err))
 db.once('open', (err) => {
   if (err) {
     throw new Error('DB not connected')
   }
   http.listen(process.env.PORT || 3000)
-  console.log('DB connected successfully at: ', Date())
+  console.log('DB connected successfully at:', Date())
 })
 
 server.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'main.html'))
 })
 
-server.post('/adduser', (req, res) => {
-  console.log(req.body)
+server.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/signup.html'))
 })
+
+server.post('/adduser', controllers.users.addUser)
 
 io.on('connection', (socket) => {
   console.log('a user connected')
